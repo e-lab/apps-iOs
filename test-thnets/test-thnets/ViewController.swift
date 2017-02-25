f527c5189c13b9b49537e2d8d9d80328e1ba576c
Eugenio <culurciello@gmail.com>
2017-02-25 11:34:15 -0500

    small thnets test added

--- test-thnets/test-thnets/ViewController.swift
+++ test-thnets/test-thnets/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: Properties:
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var button: UIButton!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +23,39 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    // MARK: Actions:
+
+    @IBAction func buttonpressed(_ sender: Any) {
+
+        // RUN THNETS TEST HERE:
+        label.text = "Success!"
+        
+        // access to neural-nets directory: http://www.techotopia.com/index.php/Working_with_Directories_in_Swift_on_iOS_8
+//        let filemgr = FileManager.default
+//        let currentPath = filemgr.currentDirectoryPath
+        
+        // neural network variable:
+        var net: UnsafeMutablePointer<THNETWORK>
+
+        THInit();
+
+        // load neural net:
+        net = THLoadNetwork("/Users/eugenioculurciello/Code/github/apps-iOs/test-thnets/neural-nets/");
+        print(net)
+
+        // run neural net:
+//        if net == nil {
+            THUseSpatialConvolutionMM(net, 2);
+            
+            label.text = "Success!"
+            
+//        } else {
+//            label.text = "CRAPPED OUT!"
+//        }
+        
     }
 
 
