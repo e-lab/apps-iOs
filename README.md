8f8fee3fc689195c0bfccbd179f36ecea448e72c
Eugenio <culurciello@gmail.com>
2017-02-26 10:52:25 -0500

    live camera added

---
+++ README.md
@@ -0,0 +1,5 @@
+# iOs applications
+
+This repo builds some application for iOs to use [Torch7](http://torch.ch/) trained neural networks.
+It uses [thnets](https://github.com/mvitez/thnets) as a backend, and does not require to compile the full Torch7 on iOs.
+Thus makes your life a lot easier!
--- test-thnets/test-thnets/ViewController.swift
+++ test-thnets/test-thnets/ViewController.swift
@@ -8,67 +8,93 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    
-    // MARK: Properties:
-    @IBOutlet weak var label: UILabel!
-    @IBOutlet weak var button: UIButton!
+import AVFoundation
 
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+class PreviewView: UIView {
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer {
+        return layer as! AVCaptureVideoPreviewLayer
     }
     
-    
-    // MARK: Actions:
-
-    @IBAction func buttonpressed(_ sender: Any) {
-        
-        // RUN THNETS TEST HERE:
-        
-        // access to neural-nets directory: http://www.techotopia.com/index.php/Working_with_Directories_in_Swift_on_iOS_8
-        // let filemgr = FileManager.default
-        // let currentPath = filemgr.currentDirectoryPath
-        
-        // neural network variable:
-        var net: UnsafeMutablePointer<THNETWORK>
-
-        THInit();
-
-        // load neural net from project:
-        let docsPath = Bundle.main.resourcePath! + "/neural-nets/"
-        
-        //test if correct file located
-        let fileManager = FileManager.default
-        do {
-            let docsArray = try fileManager.contentsOfDirectory(atPath: docsPath)
-            print(docsArray)
-        } catch {
-            print(error)
+    var session: AVCaptureSession? {
+        get {
+            return videoPreviewLayer.session
         }
-        
-        //Load Network
-        net = THLoadNetwork(docsPath)
-        print(net)
-
-        // run neural net:
-//        if net == nil {
-            THUseSpatialConvolutionMM(net, 2);
-            
-            label.text = "Success!"
-            
-//        } else {
-//            label.text = "CRAPPED OUT!"
-//        }
-        
+        set {
+            videoPreviewLayer.session = newValue
+        }
     }
-
-
+    
+    // MARK: UIView
+    
+    override class var layerClass: AnyClass {
+        return AVCaptureVideoPreviewLayer.self
+    }
 }
+
+
+//class ViewController: UIViewController {
+//    
+//    // MARK: Properties:
+//    @IBOutlet weak var label: UILabel!
+//    @IBOutlet weak var button: UIButton!
+//    @IBOutlet weak var imview: UIImageView!
+//    var newMedia: Bool?
+//
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view, typically from a nib.
+//    }
+//
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
+//    
+//    
+//    // MARK: Actions:
+//
+//    @IBAction func buttonpressed(_ sender: Any) {
+//        
+//        // RUN THNETS TEST HERE:
+//        
+//        // access to neural-nets directory: http://www.techotopia.com/index.php/Working_with_Directories_in_Swift_on_iOS_8
+//        // let filemgr = FileManager.default
+//        // let currentPath = filemgr.currentDirectoryPath
+//        
+//        // neural network variable:
+//        var net: UnsafeMutablePointer<THNETWORK>
+//
+//        THInit();
+//
+//        // load neural net from project:
+//        let docsPath = Bundle.main.resourcePath! + "/neural-nets/"
+//        
+//        //test if correct file located
+//        let fileManager = FileManager.default
+//        do {
+//            let docsArray = try fileManager.contentsOfDirectory(atPath: docsPath)
+//            print(docsArray)
+//        } catch {
+//            print(error)
+//        }
+//        
+//        //Load Network
+//        net = THLoadNetwork(docsPath)
+//        print(net)
+//
+//        // run neural net:
+////        if net == nil {
+//            THUseSpatialConvolutionMM(net, 2);
+//            
+//            label.text = "Success!"
+//            
+////        } else {
+////            label.text = "CRAPPED OUT!"
+////        }
+//        
+//    }
+//
+//
+//}
 
